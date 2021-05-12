e03ec9ead28c74ce5eeda2eb88756867bebeba3c
Entra21 <entra21@proway.treina>
2021-05-12 19:49:08 -0300

    Criado class de entrada de dados

---
+++ src/EntradaDados.java
@@ -0,0 +1,39 @@
+import java.sql.SQLOutput;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class EntradaDados {
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        int x;
+        String s1,s2,s3,s4;
+
+        x = scanner.nextInt();
+        scanner.nextLine();
+        s1 = scanner.nextLine();
+        s2 = scanner.nextLine();
+        s3 = scanner.nextLine();
+
+
+        System.out.println("DADOS DIGITADO");
+        System.out.println(x);
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+
+
+
+
+        scanner.close();
+
+    }
+
+
+
+
+
+
+}
