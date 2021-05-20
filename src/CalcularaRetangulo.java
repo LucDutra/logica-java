bf9aaff7ce7b1abea7ab272bdbb1733154bec3ae
Entra21 <entra21@proway.treina>
2021-05-20 20:52:35 -0300

    Melhorando a entrada de dados

--- src/CalcularaRetangulo.java
+++ src/CalcularaRetangulo.java
@@ -6,16 +6,18 @@ public class CalcularaRetangulo {
         Locale.setDefault(Locale.US);
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Digite o valor da base: ");
         double base = scanner.nextDouble();
+        System.out.println("Digite o valor da altura: ");
         double altura = scanner.nextDouble();
 
         double area = base * altura;
         double perimetro = 2 * base + 2 * altura;
         double diagonal = Math.sqrt(base * base + altura * altura);
 
-        System.out.println("Area: " + area);
-        System.out.println("Perimetro: " + perimetro);
-        System.out.println("Diagonal: " + diagonal);
+        System.out.printf("Area: " + "%.4f%n", area);
+        System.out.printf("Perimetro: " + "%.4f%n", perimetro);
+        System.out.printf("Diagonal: " + "%.4f", diagonal);
 
 
         scanner.close();
