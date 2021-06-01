5fc70850890520d0432e668b2132364448a3df35
Entra21 <entra21@proway.treina>
2021-06-01 20:22:35 -0300

    Alterando

--- src/ImcIF.java
+++ src/ImcIF.java
@@ -20,15 +20,17 @@ public class ImcIF {
 
         double imc = peso / (altura * altura);
 
-        if (imc <= 18.4) {
+        if (imc < 18.5) {
             System.out.println("Magreza");
-        } else if (imc >= 18.5 && imc <= 24.8) {
+        } else if (imc >= 18.5 && imc < 24.9) {
             System.out.println("Normal");
         } else if (imc >= 24.9 && imc <= 30) {
             System.out.println("Sobrepeso");
-        } else if (imc > 30.0) {
+        } else{
             System.out.println("Obesidade");
         }
+
+        System.out.println(imc);
 
         scanner.close();
 
